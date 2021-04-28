1ef0cf57d3ca5c8a312cf6578ba595a9b13ac9da
Jesus Caballero <voraphi@outlook.com>
2021-04-27 23:09:58 -0700

    user details

--- Diss-Hits/UserDetailsViewController.swift
+++ Diss-Hits/UserDetailsViewController.swift
@@ -12,12 +12,26 @@ import SwiftUI
 
 class UserDetailsViewController: UIViewController {
 
+    @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var userProfileImage: UIImageView!
+    @IBOutlet weak var userBio: UILabel!
+    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        userDeatails()
 
         // Do any additional setup after loading the view.
         
         
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+//        print("oiadoiwajodiwjaoidwoaj")
+//        self.userDeatails()
     }
     
     @IBAction func logoutButton(_ sender: Any) {
@@ -28,6 +42,47 @@ class UserDetailsViewController: UIViewController {
         
         PFUser.logOut();
         delegate.window?.rootViewController = loginView;
+    }
+    
+    func userDeatails() {
+        
+        let userData = appDelegate.getUserData();
+        let query = PFQuery(className:"_User");
+        query.whereKey("GID", equalTo: userData.userId);
+        
+        query.findObjectsInBackground { (results: [PFObject]?, error: Error?)
+            in
+            if let error = error {
+                //could not find user
+                print(error.localizedDescription);
+            } else {
+                
+                if let results = results {
+                    print(results)
+                    
+//                    let username = results[0]["username"]
+                    
+                    if let imageFile = results[0]["userPfp"] as? PFFileObject {
+                        let urlString = imageFile.url!
+                        let url = URL(string: urlString)!
+                        
+                        self.userProfileImage.af.setImage(withURL: url)
+                    }
+                    
+                    self.username.text = results[0]["username"] as! String
+                    
+                    self.userBio.text = results[0]["Bio"] as! String
+                    
+                    
+                    
+                    
+                   
+                
+               }
+                
+            }
+        }
+        
     }
     
     /*
