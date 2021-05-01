1a36bb43a4200f1e075c0b380f6f27748a9475e3
Jesus Caballero <voraphi@outlook.com>
2021-04-30 17:23:20 -0700

    UIDocumentPicker WIP

--- Diss-Hits/UploadSongViewController.swift
+++ Diss-Hits/UploadSongViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 import Parse
 import MediaPlayer
 import AlamofireImage
+import MobileCoreServices
+import UniformTypeIdentifiers
 
-class UploadSongViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, MPMediaPickerControllerDelegate {
+class UploadSongViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, MPMediaPickerControllerDelegate, UIDocumentMenuDelegate, UIDocumentPickerDelegate
+ {
 
     @IBOutlet weak var imageView: UIImageView!
+    weak var documentPicker: UIDocumentPickerViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,26 +56,67 @@ class UploadSongViewController: UIViewController, UIImagePickerControllerDelegat
     }
     
     @IBAction func selectSongs(_ sender: UIButton) {
-        let controller = MPMediaPickerController(mediaTypes: .music)
-        controller.allowsPickingMultipleItems = true
-        controller.popoverPresentationController?.sourceView = sender
-        controller.delegate = self
-        present(controller, animated: true)
+//        let controller = MPMediaPickerController(mediaTypes: .music)
+//        controller.allowsPickingMultipleItems = true
+//        controller.popoverPresentationController?.sourceView = sender
+//        controller.delegate = self
+//        present(controller, animated: true)
+        let types = [kUTTypeMP3, kUTTypeWaveformAudio, kUTTypeAudioInterchangeFileFormat, kUTTypeMIDIAudio, kUTTypeJPEG]
+        let importMenu = UIDocumentMenuViewController(documentTypes: types as [String], in: .import)
+            importMenu.delegate = self
+            importMenu.modalPresentationStyle = .formSheet
+            self.present(importMenu, animated: true, completion: nil)
+        
+        
     }
     
-    func mediaPicker(_ mediaPicker: MPMediaPickerController,
-                     didPickMediaItems mediaItemCollection: MPMediaItemCollection) {
-        // Get the system music player.
-        let musicPlayer = MPMusicPlayerController.systemMusicPlayer
-        musicPlayer.setQueue(with: mediaItemCollection)
-        mediaPicker.dismiss(animated: true)
-        // Begin playback.
-        musicPlayer.play()
+    public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        guard let myURL = urls.first else {
+            return
+        }
+        print("import result : \(myURL)")
+    }
+          
+
+    public func documentMenu(_ documentMenu:UIDocumentMenuViewController, didPickDocumentPicker documentPicker: UIDocumentPickerViewController) {
+        documentPicker.delegate = self
+        present(documentPicker, animated: true, completion: nil)
     }
 
-    func mediaPickerDidCancel(_ mediaPicker: MPMediaPickerController) {
-        mediaPicker.dismiss(animated: true)
+
+    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        print("view was cancelled")
+        dismiss(animated: true, completion: nil)
     }
+    
+    override public func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
+        if self.presentedViewController == nil && self.documentPicker != nil {
+            self.documentPicker = nil
+        }else{
+            super.dismiss(animated: flag, completion: completion)
+        }
+    }
+
+    public override func present(_ viewControllerToPresent: UIViewController, animated flag: Bool, completion: (() -> Void)? = nil) {
+        if viewControllerToPresent is UIDocumentPickerViewController {
+            self.documentPicker = viewControllerToPresent as? UIDocumentPickerViewController
+        }
+        super.present(viewControllerToPresent, animated: flag, completion: completion)
+    }
+    
+//    func mediaPicker(_ mediaPicker: MPMediaPickerController,
+//                     didPickMediaItems mediaItemCollection: MPMediaItemCollection) {
+//        // Get the system music player.
+//        let musicPlayer = MPMusicPlayerController.systemMusicPlayer
+//        musicPlayer.setQueue(with: mediaItemCollection)
+//        mediaPicker.dismiss(animated: true)
+//        // Begin playback.
+//        musicPlayer.play()
+//    }
+//
+//    func mediaPickerDidCancel(_ mediaPicker: MPMediaPickerController) {
+//        mediaPicker.dismiss(animated: true)
+//    }
     
 
     /*
