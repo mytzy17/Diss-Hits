7ccb2add7324f2b924db005a3905d43c439cb210
Jennifer Lopez <jenniferlopezh0@gmail.com>
2021-05-01 21:07:02 -0700

    displays user information on segmented view

--- Diss-Hits/ArtistActivityViewController.swift
+++ Diss-Hits/ArtistActivityViewController.swift
@@ -13,42 +13,49 @@ import AlamofireImage
 class ArtistActivityViewController: UIViewController {
     
     @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var pfpView: UIImageView!
+    @IBOutlet weak var usernameView: UILabel!
+    @IBOutlet weak var bioView: UILabel!
     
-//    var artist:[String:Any]!
     var artist = PFUser()
-//    var artist = PFObject()
-//    let albumCover = song["albumCover"] as? PFFileObject
     
-    @IBAction func segmentControlAction(_ sender: UISegmentedControl) {
-        switch segmentControl.selectedSegmentIndex {
-            case 0:
-                print("SONGS")
-            case 1:
-                print("PLAYLISTS")
-            default:
-                break
-        }
-    }
-    //
-//    @IBAction func segmentControlAction(_ sender: UISegmentedControl) {
-//        switch segmentControl.selectedSegmentIndex {
-//            case 0:
-//                label.text = "Artists' songs"
-//            case 1:
-//                label.text = "Playlists"
-//            default:
-//                break
-//        }
-//    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print("NEW SCREEN")
-        print(artist)
+        // print("NEW SCREEN")
+        // print(artist)
+        
+        usernameView.text = artist.username
+        let bioString = artist["Bio"] as! String
+        bioView.text = bioString
+        
+        displayingSongs()
     }
     
+    @IBAction func segmentControlAction(_ sender: UISegmentedControl) {
+        switch segmentControl.selectedSegmentIndex {
+            case 0:
+                displayingSongs()
+            case 1:
+                displayingPlaylists()
+            default:
+                break
+        }
+    }
 
+    func displayingSongs() {
+        print("SONGS YAY")
+        print(artist.objectId)
+        
+        // will use object id to query artist songs
+        // similar query user details
+    }
+    
+    func displayingPlaylists() {
+        print("PLAYLISTS WOW")
+        print(artist.objectId)
+    }
     /*
     // MARK: - Navigation
 
