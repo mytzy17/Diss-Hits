dcbe2113339fbcfbf30f8fdb29f94734a452e541
Jose Alfaro <joalfaro@csumb.edu>
2021-05-03 19:22:11 -0700

    more edit stuff

--- Diss-Hits/EditProfileViewController.swift
+++ Diss-Hits/EditProfileViewController.swift
@@ -7,15 +7,21 @@
 
 import UIKit
 import Parse
+import MobileCoreServices
+import AlamofireImage
+import UniformTypeIdentifiers
 
-
-class EditProfileViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+class EditProfileViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate & UINavigationControllerDelegate, UIDocumentMenuDelegate, UIDocumentPickerDelegate {
+   
+    
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var bioLabel: UILabel!
     @IBOutlet weak var bioField: UITextField!
+    weak var documentPicker: UIDocumentPickerViewController?
+
     
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
     
@@ -43,18 +49,11 @@ class EditProfileViewController: UIViewController, UITextFieldDelegate, UIImageP
       }
 
     @IBAction func onProfileImage(_ sender: Any) {
-        let picker = UIImagePickerController()
-            picker.delegate = self
-            picker.allowsEditing = true
-
-            if UIImagePickerController.isSourceTypeAvailable(.camera){
-                picker.sourceType = .camera
-
-            } else {
-                picker.sourceType = .photoLibrary
-            }
-
-            present(picker, animated: true, completion: nil)
+        let types = [kUTTypePNG, kUTTypeJPEG, kUTTypeImage, kUTTypeJPEG2000, kUTTypeRawImage]
+        let importMenu = UIDocumentMenuViewController(documentTypes: types as [String], in: .import)
+            importMenu.delegate = self
+            importMenu.modalPresentationStyle = .formSheet
+            self.present(importMenu, animated: true, completion: nil)
     }
     
     func userDeatails() {
@@ -120,5 +119,45 @@ class EditProfileViewController: UIViewController, UITextFieldDelegate, UIImageP
 
             currentUser!.saveInBackground()
         }
+    }
+    
+    public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        guard let myURL = urls.first else {
+            return
+        }
+        print("import result : \(myURL)")
+        
+        guard let image = UIImage(contentsOfFile: myURL.path) else {
+            print("Not image file")
+            return
+        }
+        imageView.image = image
+    }
+          
+
+    public func documentMenu(_ documentMenu:UIDocumentMenuViewController, didPickDocumentPicker documentPicker: UIDocumentPickerViewController) {
+        documentPicker.delegate = self
+        present(documentPicker, animated: true, completion: nil)
+    }
+
+
+    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        print("view was cancelled")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override public func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
+        if self.presentedViewController == nil && self.documentPicker != nil {
+            self.documentPicker = nil
+        }else{
+            super.dismiss(animated: flag, completion: completion)
+        }
+    }
+
+    public override func present(_ viewControllerToPresent: UIViewController, animated flag: Bool, completion: (() -> Void)? = nil) {
+        if viewControllerToPresent is UIDocumentPickerViewController {
+            self.documentPicker = viewControllerToPresent as? UIDocumentPickerViewController
+        }
+        super.present(viewControllerToPresent, animated: flag, completion: completion)
     }
 }
